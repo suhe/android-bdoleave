80818751aa063d9101dbb5f17e60815b2b957ca5
suhe <hendarsyahss@gmail.com>
2016-02-15 17:36:51 +0700

    Add login validation

--- app/src/main/java/com/example/bdo/androidbdoleave/LoginActivity.java
+++ app/src/main/java/com/example/bdo/androidbdoleave/LoginActivity.java
@@ -5,26 +5,76 @@ package com.example.bdo.androidbdoleave;
  */
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends Activity {
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.Validator.ValidationListener;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+
+import java.util.List;
+
+public class LoginActivity extends Activity implements ValidationListener {
+
+    @NotEmpty(sequence = 1, message = "Please NIK Do Not Empty")
+    EditText edNik;
+    @NotEmpty(sequence = 2, message = "Please Password Do Not Empty")
+    EditText edPassword;
     Button btnLogin;
+    TextView txtresult;
+
+    private Validator mValidator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        edNik = (EditText) findViewById(R.id.nik);
+        edPassword = (EditText) findViewById(R.id.password);
+        txtresult = (TextView) findViewById(R.id.resultTextView);
+
+        // Validator
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
+
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                mValidator.validate();
             }
         });
+    }
 
+    @Override
+    public void onValidationSucceeded() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (ValidationError error : errors) {
+            stringBuilder.append(error.getCollatedErrorMessage(this))
+                    .append('\n');
+        }
+        txtresult.setText(stringBuilder.toString().trim());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
--- app/src/main/java/com/example/bdo/androidbdoleave/MainActivity.java
+++ app/src/main/java/com/example/bdo/androidbdoleave/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_change_password) {
             return true;
         }
 
